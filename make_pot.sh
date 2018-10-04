5066b14c9e7e12839da8d5dcaeffa52684189d4c
Jibec <jean-baptiste@holcroft.fr>
2018-10-04 07:40:28 +0200

    Add pot/po generation

---
+++ make_pot.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# You need po4a > 0.54, see https://github.com/mquinson/po4a/releases
+# There is no need of system-wide installation of po4a
+# Usage: PERLLIB=/path/to/po4a/lib make_pot.sh
+# you may set following variables
+# SRCDIR root of the documentation repository
+# POTDIR place where to create the pot
+
+
+####################################
+# INITILIZE VARIABLES
+####################################
+
+# root of the documentation repository
+if [ -z "$SRCDIR" ] ; then
+	SRCDIR="./" # no trailing slash
+fi
+SRCDIR_MODULE="$SRCDIR/content/fr"
+
+# place where to create the pot files
+if [ -z "$POTDIR" ] ; then
+	POTDIR="./l10n/pot"
+fi
+
+
+####################################
+# TEST IF IT CAN WORK
+####################################
+
+if [ ! -d "$SRCDIR_MODULE" ] ; then
+    echo "Error, please check that SRCDIR match to the root of the documentation repository"
+    echo "You specified modules are in $SRCDIR_MODULE"
+    exit 1
+fi
+
+
+####################################
+# HANDLE articles and pages
+####################################
+
+while IFS= read -r -d '' file
+do
+    basename=$(basename -s .md "$file")
+    dirname=$(dirname "$file")
+    path="${dirname#$SRCDIR_MODULE/}"
+
+    if [ "$dirname" = "$SRCDIR_MODULE" ]; then
+        potname=$basename.pot
+    else
+        potname=$path/$basename.pot
+        mkdir -p "$POTDIR/$path"
+    fi
+
+    po4a-gettextize \
+    	--format asciidoc --option debug split_attributelist \
+    	--master "$file" --master-charset "UTF-8" \
+    	--po "$POTDIR/$potname"
+
+done <   <(find -L "$SRCDIR_MODULE" -name "*.md" -print0)
---
+++ use_po.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# You need po4a > 0.54, see https://github.com/mquinson/po4a/releases
+# There is no need of system-wide installation of po4a
+# Usage: PERLLIB=/path/to/po4a/lib use_po.sh
+# you may set following variables
+# SRCDIR root of the documentation repository
+# PODIR place where to create the po
+# PUB_DIR place where to publish localized files
+
+####################################
+# INITILIZE VARIABLES
+####################################
+
+# root of the documentation repository
+if [ -z "$SRCDIR" ] ; then
+	SRCDIR="./" # no trailing slash
+fi
+SRCDIR=$(realpath "$SRCDIR")
+SRCDIR_MODULE="$SRCDIR/content/fr"
+
+# place where the po files are
+if [ -z "$PO_DIR" ] ; then
+	PO_DIR="./l10n/po"
+fi
+
+# place where the localized files will be
+if [ -z "$PUB_DIR" ] ; then
+	PUB_DIR="./content/"
+fi
+
+
+####################################
+# TEST IF IT CAN WORK
+####################################
+
+if [ ! -d "$SRCDIR_MODULE" ] ; then
+	echo "Please run this script from the documentation' root folder"
+	exit 1
+fi
+
+
+####################################
+# DEFINE ANTARA MODULES (md files)
+####################################
+
+use_po_module () {
+	lang=$1
+
+	while IFS= read -r -d '' file
+	do
+		basename=$(basename -s .md "$file")
+		dirname=$(dirname "$file")
+		path="${dirname#$SRCDIR_MODULE/}"
+
+		if [ "$dirname" = "$SRCDIR_MODULE" ]; then
+			potname=$basename
+			localized_file="$PUB_DIR/$lang/$basename.md"
+		else
+			potname=$path/$basename
+			localized_file="$PUB_DIR/$lang/$path/$basename.md"
+		fi
+
+		if [ ! -e "$PO_DIR/$lang/$potname.po" ]; then
+			po4a-gettextize --format asciidoc --option debug split_attributelist \
+				--master "$file" --master-charset "UTF-8" \
+				--po "$PO_DIR/$lang/$potname.po"
+		fi
+
+		po4a-translate --format asciidoc --option debug split_attributelist \
+			--master "$file" --master-charset "UTF-8" \
+			--po "$PO_DIR/$lang/$potname.po" \
+			--localized "$localized_file" --localized-charset "UTF-8" \
+			--keep 0
+	done <   <(find -L "$SRCDIR_MODULE" -name "*.md"  -print0)
+}
+
+####################################
+# HANDLE ANTARA MODULES (md files)
+####################################
+
+while IFS= read -r -d '' dir
+do
+	lang=$(basename -s .md "$dir")
+	echo "$lang"
+	use_po_module "$lang"
+done <   <(find "$PO_DIR" -mindepth 1 -maxdepth 1 -type d -print0)
